31f84cedec663cf4dd1381ac54ed7002d128e506
Bhawna Gupta <bhawna.gupta@v2solutions.com>
2021-06-14 17:16:08 +0530

    js changes

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 // Function to show user data
 
 function showUserData() {
-    console.log(document.getElementById("firstname").innerHTML);
+    
+    var firstName1 = document.getElementById("firstName").value;
+    var lastName1 = document.getElementById("lastName").value;
+    var gender1 = document.getElementById("gender").value;
+
+    let data = document.getElementById("result");
+    data.innerHTML = 
+    ` <p>First Name : ${firstName1}</p>
+    <p>Last Name: ${lastName1}</p>
+    <p>Gender : ${gender1}</p> `
+    
+    //document.write ( "First Name : " + document.getElementById("firstName").value + "<br>" + "Last Name : " + document.getElementById("lastName").value + "<br>" + "Gender : " + document.getElementById("gender").value);
+
+
+    console.log("First Name : " + document.getElementById("firstName").value);
+    console.log("Last Name : " +  document.getElementById("lastName").value);
+    console.log("Gender : " +  document.getElementById("gender").value);
+    // showData(firstName1, lastName1, gender1);
 }
+
+// function showData(firstName1, lastName1, gender1) {
+//     let data = document.getElementById("result");
+//     data.innerHTML = 
+//     ` <p>Fist Name : ${firstName1}</p>
+//     <p>Last Name: ${lastName1}</p>
+//     <p>Gender : ${gender1}</p> `
+// }
