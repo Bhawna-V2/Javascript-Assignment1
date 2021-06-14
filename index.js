fe1ef29ce5f8102d3d76b3731e255ad563a307b1
Bhawna Gupta <bhawna.gupta@v2solutions.com>
2021-06-14 17:48:49 +0530

    array practice

--- index.js
+++ index.js
@@ -28,3 +28,61 @@ function showUserData() {
 //     <p>Last Name: ${lastName1}</p>
 //     <p>Gender : ${gender1}</p> `
 // }
+
+
+// Array Practice
+
+function showUsersData() {
+    var names = [
+        {
+            name: "Bhawna Gupta",
+            id: 101,
+            position: "Front End Developer",
+            salary: 35000,
+        },
+        {
+            name: "Praveen Kumar",
+            id: 102,
+            position: "Messaging Administartor",
+            salary: 55000,
+        },
+        {
+            name: "Mahi Garg",
+            id: 103,
+            position: "Sr. Software Engineer",
+            salary: 85000,
+        },
+        {
+            name: "Mahir Garg",
+            id: 104,
+            position: "Jr. Software Engineer",
+            salary: 15000,},
+        {
+            name: "Ansh Gupta",
+            id: 105,
+            position: "Jr. Front End Developer",
+            salary: 25000,
+        },
+    ]
+
+
+    for(var i=0; i < names.length; i++) {
+
+        if (names[i].salary > 30000) {
+            console.log(names[i].salary);
+
+            //document.write (names[i].salary + " , ")
+            
+            let stat = document.getElementById("userStat");
+            stat.innerHTML = 
+            `<p> ${names[i].salary} </p>`;
+        } else {
+            //document.write (names[i].salary + " , ")
+        }
+        
+        //console.log(names[i]);
+    }
+
+   
+}
+
